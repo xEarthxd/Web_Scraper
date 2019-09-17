aac124751f9faaaa6673b0c10809bcd0f713300c
xEarthxd <di_stat@hotmail.com>
2019-09-17 15:25:39 +0700

    first commit

---
+++ src/main/java/com/WebScraping/Scraper/ScraperApplication.java
@@ -0,0 +1,13 @@
+package com.WebScraping.Scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WebScraping/Scraper/ScraperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.WebScraping.Scraper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScraperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
